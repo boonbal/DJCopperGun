c5f42aba6fb2b7b5fb680b347e159efa14f2b023
임영준 <limyjoon1117@gmail.com>
2025-03-29 03:13:15 +0900

    first commit

---
+++ pingpong.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', message => {
+    if (message.content === '!핑') {
+        message.reply('퐁!');
+    }
+});
+
+client.login(process.env.TOKEN);
